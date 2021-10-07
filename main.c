5017b1ff3a10f113de1d003b159910faf50d83dd
xiphiasnonus <foster.pianist@gmail.com>
2021-10-07 15:44:08 -0400

    initial commit

---
+++ main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <math.h>
+
+#define NUM_NOTES 12
+
+// calculate a frequency (Hz) that is 'n' cents offset from frequenct 'a' (hz)
+double frequency_offset(double a, double n)
+{
+    return a * pow(2.0, n / 1200.0);
+}
+
+// returns the difference, in cents, of frequencies 'lower' and 'higher' (Hz)
+double cents_delta(double lower, double higher)
+{
+    return 1200.0 * log2(lower / higher);
+}
+
+// round to two decimal places
+double round2(double freq)
+{
+    return round(freq * 1000.0) / 1000.0;
+}
+
+int main(int argc, char ** argv)
+{
+    if ( argc != 2 ) {
+        printf("usage: %s [input file]\n", argv[0]);
+        return 1;
+    }
+        
+    FILE * file = fopen(argv[1], "r");
+    if ( file == NULL ) {
+        printf("could not open %s!\n", argv[1]);
+        return 1;
+    }
+    
+    // calculate ET frequencies
+    
+    double et_freq[NUM_NOTES]; // from A4 to G sharp 4
+    for ( int i = 0; i < NUM_NOTES; i++ ) {
+        int index = (i + 9) % NUM_NOTES;
+        et_freq[index] = 440.0 * pow(2.0, (double)i / 12.0);
+    }
+    
+    // calculate temperament freqencies given offsets in file
+    
+    double freq[NUM_NOTES];
+    for ( int i = 0; i < NUM_NOTES - 1; i++ ) {
+        int lower;
+        int upper;
+        double offset_cents;
+        
+        fscanf(file, "%d-%d %lf\n", &lower, &upper, &offset_cents);
+        if ( i == 0 ) {
+            freq[lower] = et_freq[lower];
+        }
+        
+        freq[upper] = frequency_offset(freq[lower] * 1.5, offset_cents);
+    }
+    
+    // calculate and print cent offsets from ET of temperament frequencies
+    
+    for ( int i = 0; i < NUM_NOTES; i++ ) {
+        
+        // move all frequencies into one octave range
+        while ( freq[i] < 440.0 )
+            freq[i] *= 2.0;
+        while ( freq[i] >= 880.0 )
+            freq[i] /= 2.0;
+        
+        printf("pitch %2d: (ET: %3.2lf Hz) (T: %3.2lf Hz), delta: %+3.2lf cents\n",
+               i,
+               et_freq[i],
+               freq[i],
+               round2(cents_delta(freq[i], et_freq[i])));
+    }
+    
+    fclose(file);
+    return 0;
+}
