21962534dc96b32c165d9df1ac1a12d550551ee4
xiphiasnonus <foster.pianist@gmail.com>
2021-11-05 19:35:37 -0400

    add outputing to file

--- main.c
+++ main.c
@@ -18,20 +18,32 @@ double cents_delta(double lower, double higher)
 // round to two decimal places
 double round2(double freq)
 {
-    return round(freq * 1000.0) / 1000.0;
+    return round(freq * 100.0) / 100.0;
 }
 
 int main(int argc, char ** argv)
 {
-    if ( argc != 2 ) {
-        printf("usage: %s [input file]\n", argv[0]);
+    if ( argc < 2 ) {
+        printf("usage: %s [input file] ([output file])\n", argv[0]);
+        printf("Data is output to console if no output file is specified.\n");
         return 1;
     }
         
-    FILE * file = fopen(argv[1], "r");
-    if ( file == NULL ) {
+    FILE * input = fopen(argv[1], "r");
+    if ( input == NULL ) {
         printf("could not open %s!\n", argv[1]);
         return 1;
+    }
+    
+    FILE * output;
+    if ( argc == 3 ) { // user specified output file
+        output = fopen(argv[2], "w");
+        if ( output == NULL ) {
+            fprintf(stderr, "Error: failed to create file %s\n", argv[2]);
+            return 1;
+        }
+    } else {
+        output = stdout;
     }
     
     // calculate ET frequencies
@@ -50,7 +62,7 @@ int main(int argc, char ** argv)
         int upper;
         double offset_cents;
         
-        fscanf(file, "%d-%d %lf\n", &lower, &upper, &offset_cents);
+        fscanf(input, "%d-%d %lf\n", &lower, &upper, &offset_cents);
         if ( i == 0 ) {
             freq[lower] = et_freq[lower];
         }
@@ -68,13 +80,13 @@ int main(int argc, char ** argv)
         while ( freq[i] >= 880.0 )
             freq[i] /= 2.0;
         
-        printf("pitch %2d: (ET: %3.2lf Hz) (T: %3.2lf Hz), delta: %+3.2lf cents\n",
-               i,
-               et_freq[i],
-               freq[i],
-               round2(cents_delta(freq[i], et_freq[i])));
+        const char * format = "pitch %2d: (ET: %3.2lf Hz) (T: %3.2lf Hz), delta: %+3.2lf cents\n";
+        double rounded_delta = round2(cents_delta(freq[i], et_freq[i]));
+        
+        fprintf(output, format, i, et_freq[i], freq[i], rounded_delta);
     }
     
-    fclose(file);
+    fclose(input);
+    fclose(output);
     return 0;
 }
